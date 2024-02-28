dcf5e1f26af918bc3cc35c8d90b73b235438544d
Mou <madalmou.2004@gmail.com>
2024-02-29 00:19:44 +0530

    clock folder

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import  clock  from './clock.vue';
+</script>
+
+<template>
+  <clock/>
+</template>
+
---
+++ src/clock.vue
@@ -0,0 +1,32 @@
+<script setup>
+let times;
+const func = () => {
+    times = setInterval(() => {
+        const d = new Date();
+        const newh = d.getHours();
+        const newm = d.getMinutes();
+        const news = d.getSeconds();
+        const hrotation = 30 * newh + newm / 2;
+        const mrotation = 6 * newm;
+        const srotation = 6 * news;
+        hour.style.transform = `rotate(${hrotation}deg)`;
+        minute.style.transform = `rotate(${mrotation}deg)`;
+        second.style.transform = `rotate(${srotation}deg)`;
+    }, 1000);
+}
+const copy = () => {
+    clearInterval(times);
+}
+
+</script>
+
+<template>
+    <div id="container">
+        <div id="hour"></div>
+        <div id="minute"></div>
+        <div id="second"></div>
+    </div>
+    <button @click="func">start</button>
+    <button @click="copy">stop</button>
+</template>
+
