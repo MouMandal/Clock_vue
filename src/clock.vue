6d3129e3e34f4f1a9b99fdd667a33ade59020398
Mou <madalmou.2004@gmail.com>
2024-02-29 23:22:30 +0530

    modify clock folder

--- src/clock.vue
+++ src/clock.vue
@@ -1,22 +1,22 @@
 <script setup>
 let times;
-const func = () => {
-    times = setInterval(() => {
-        const d = new Date();
-        const newh = d.getHours();
-        const newm = d.getMinutes();
-        const news = d.getSeconds();
-        const hrotation = 30 * newh + newm / 2;
-        const mrotation = 6 * newm;
-        const srotation = 6 * news;
-        hour.style.transform = `rotate(${hrotation}deg)`;
-        minute.style.transform = `rotate(${mrotation}deg)`;
-        second.style.transform = `rotate(${srotation}deg)`;
-    }, 1000);
-}
-const copy = () => {
-    clearInterval(times);
-}
+// const func = () => {
+times = setInterval(() => {
+    const d = new Date();
+    const newh = d.getHours();
+    const newm = d.getMinutes();
+    const news = d.getSeconds();
+    const hrotation = 30 * newh + newm / 2;
+    const mrotation = 6 * newm;
+    const srotation = 6 * news;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+// }
+// const copy = () => {
+//     clearInterval(times);
+// }
 
 </script>
 
@@ -26,7 +26,7 @@ const copy = () => {
         <div id="minute"></div>
         <div id="second"></div>
     </div>
-    <button @click="func">start</button>
-    <button @click="copy">stop</button>
+    <!-- <button @click="func">start</button>
+    <button @click="copy">stop</button> -->
 </template>
 
